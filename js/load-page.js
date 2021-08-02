38b7356eb026921e22b560f982236abb7082fa05
pauloAlvaresNeto <paulo.alvares01@etec.sp.gov.br>
2021-08-02 12:29:07 -0300

    commit de Atualização das Janelas Modais

--- js/load-page.js
+++ js/load-page.js
@@ -6,7 +6,43 @@ $(document).ready(function(){
 $('a').click(function(e){
     e.preventDefault()
 
-    $('#modal-info').modal('show')
-    })
+    // Criar uma variável local que receba o atributo href do link
+    let page = $(this).attr('href')
 
+    $('.modal-title').empty() //Limpar
+    $('.modal-body').empty() //Limpar
+
+    // Verificar qual conteúdo eu devo carregar
+    switch (page){
+       
+      // Se for para abrir a page Sobre Min:
+      case 'about': 
+          $('.modal-title').append('<strong>Sobre Min</strong>')
+          $('.modal-body').append(`<p>Me chamo Paulo Alvares Neto, nasci em 2005 na cidade de Lins-SP, e cresci e moro em Getulina-SP</p>`)
+          $('#modal-info').modal('show')
+        break
+        
+          // Se for para abrir a pagina Parcerias:
+      case 'partners': 
+          $('.modal-title').append('<strong>Parcerias</strong>')
+          $('.modal-body').append(`<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quidem asperiores ipsa neque tenetur libero facere, corporis et magni pariatur dignissimos obcaecati voluptate consequuntur ipsam provident dolor odio doloribus quaerat.</p>`)
+          $('#modal-info').modal('show')
+        break
+        
+        // Se for para abrir a pagina Trabalhos:
+        case 'jobs': 
+          $('.modal-title').append('<strong>Trabalhos</strong>')
+          $('.modal-body').append(`<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quidem asperiores ipsa neque tenetur libero facere, corporis et magni pariatur dignissimos obcaecati voluptate consequuntur ipsam provident dolor odio doloribus quaerat.</p>`)
+          $('#modal-info').modal('show')
+          break
+        
+          // Se for para abrir a pagina Contatos:
+        case 'contacts': 
+          $('.modal-title').append('<strong>Contatos</strong>')
+          $('.modal-body').append(`<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi quidem asperiores ipsa neque tenetur libero facere, corporis et magni pariatur dignissimos obcaecati voluptate consequuntur ipsam provident dolor odio doloribus quaerat.</p>`)
+          $('#modal-info').modal('show')
+        break
+    }
+
+  })
 })
